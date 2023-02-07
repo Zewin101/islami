c1cce752a3bef25551b1e3c957e2bc98637a088e
Zewin <01200361136r@gmail.com>
2023-02-07 04:47:51 +0300

    splash screen

---
+++ lib/layout_screen/layout.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LayoutScreen extends StatelessWidget {
+  const LayoutScreen({Key? key}) : super(key: key);
+static const String routeName="layout";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
